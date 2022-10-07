2cf15b981b390a7855f3698a275e76964450ba50
sort7er <98805454+sort7er@users.noreply.github.com>
2022-10-07 14:58:29 +0200

    Lift

---
+++ Assets/_Scripts/Temp_Scripts/ParentToLift.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class ParentToLift : MonoBehaviour
+{
+    private Rigidbody playerRB;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = transform;
+            playerRB = other.GetComponent<Rigidbody>();
+            playerRB.useGravity = false;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.transform.parent = null;
+            playerRB = null;
+            playerRB.useGravity = true;
+        }
+    }
+}
