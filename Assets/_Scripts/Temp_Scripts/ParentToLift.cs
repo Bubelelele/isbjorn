20b2351292016b6bd1d3edba29a551b0f83c60a2
Bubelelele <89357128+Bubelelele@users.noreply.github.com>
2022-10-10 23:53:57 +0200

    Revert "Merge branch 'main2' of https://github.com/Bubelelele/isbjorn into main2"
    This reverts commit 8ae630fc6c9a8a4d9b611cea6de21bbf22a3aa46, reversing
    changes made to 48ad42ac0779d09af68f2bbc22d2590df694e359.

--- Assets/_Scripts/Temp_Scripts/ParentToLift.cs
+++ Assets/_Scripts/Temp_Scripts/ParentToLift.cs
@@ -1,16 +1,43 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ParentToLift : MonoBehaviour
 {
     private Rigidbody playerRB;
-    private Rigidbody rb;
+    private Rigidbody _platformRigidbody;
+    private Vector3 _platformPositionLastFrame;
+    
+    [SerializeField] private List<Rigidbody> rigidbodiesOnPlatform = new List<Rigidbody>();
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
+        _platformRigidbody = GetComponent<Rigidbody>();
     }
+
+    private void FixedUpdate()
+    {
+        _platformPositionLastFrame = _platformRigidbody.position;
+        foreach (Rigidbody rigidbody in rigidbodiesOnPlatform)
+        {
+            MoveRigidbodiesOnPlatform(rigidbody);
+        }
+    }
+
+    private void MoveRigidbodiesOnPlatform(Rigidbody rigidbody)
+    {
+        rigidbody.position += _platformRigidbody.position - _platformPositionLastFrame;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        if (other.attachedRigidbody != null)
+        {
+            if (!rigidbodiesOnPlatform.Contains(other.attachedRigidbody))
+                rigidbodiesOnPlatform.Add(other.attachedRigidbody);
+        }
+
+        return;
         if (other.gameObject.CompareTag("Player"))
         {
             other.transform.parent = transform;
@@ -19,6 +46,13 @@ public class ParentToLift : MonoBehaviour
     }
     private void OnTriggerExit(Collider other)
     {
+        if (other.attachedRigidbody != null)
+        {
+            if (rigidbodiesOnPlatform.Contains(other.attachedRigidbody))
+                rigidbodiesOnPlatform.Remove(other.attachedRigidbody);
+        }
+
+        return;
         if (other.gameObject.CompareTag("Player"))
         {
             other.transform.parent = null;
