558642b6a4ff4e0813d96416dc9f2adf0e85f148
Granderous <kevin.b.grande@student.nord.no>
2022-11-04 01:45:16 +0100

    Hunger System And Walrus Track Food

---
+++ Assets/_Scripts/Player/HungerSystem/PlayerHunger.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHunger : MonoBehaviour
+{
+
+    [Header("Player Hunger")]
+
+    public float maxHunger = 100;
+    public float hunger;
+    public float hungerOverTime;
+    public Slider hungerSlider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hunger = hunger - hungerOverTime * Time.deltaTime;
+
+        if (hunger <= 0)
+        {
+            hunger = 0;
+        }
+
+        UpdateSliders();
+    }
+
+    public void AddFood(float Amount)
+    {
+        this.hunger += Amount; 
+    }
+
+
+    void UpdateSliders()
+    {
+        hungerSlider.value = hunger / maxHunger; 
+    }
+}
---
+++ Assets/_Scripts/Systems/AI/FoodIsDroppedChecker.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FoodIsDroppedChecker : MonoBehaviour
+{
+    public bool foodIsDropped;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foodIsDropped = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
