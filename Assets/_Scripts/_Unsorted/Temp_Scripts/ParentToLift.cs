4425c17192e827bdd184cbcea9b4327478f77ae9
sort7er <98805454+sort7er@users.noreply.github.com>
2022-11-04 14:49:17 +0100

    changed the pendulum prefab

--- Assets/_Scripts/_Unsorted/Temp_Scripts/ParentToLift.cs
+++ Assets/_Scripts/_Unsorted/Temp_Scripts/ParentToLift.cs
@@ -4,59 +4,59 @@ using UnityEngine;
 
 public class ParentToLift : MonoBehaviour
 {
-    private Rigidbody playerRB;
-    private Rigidbody _platformRigidbody;
-    private Vector3 _platformPositionLastFrame;
+    //private Rigidbody playerRB;
+    //private Rigidbody _platformRigidbody;
+    ////private Vector3 _platformPositionLastFrame;
 
-    [SerializeField] private List<Rigidbody> rigidbodiesOnPlatform = new List<Rigidbody>();
+    //[SerializeField] private List<Rigidbody> rigidbodiesOnPlatform = new List<Rigidbody>();
 
-    private void Start()
-    {
-        _platformRigidbody = GetComponent<Rigidbody>();
-    }
+    //private void Start()
+    //{
+    //    _platformRigidbody = GetComponent<Rigidbody>();
+    //}
 
-    private void FixedUpdate()
-    {
-        _platformPositionLastFrame = _platformRigidbody.position;
-        foreach (Rigidbody rigidbody in rigidbodiesOnPlatform)
-        {
-            MoveRigidbodiesOnPlatform(rigidbody);
-        }
-    }
+    //private void FixedUpdate()
+    //{
+    //    _platformPositionLastFrame = _platformRigidbody.position;
+    //    foreach (Rigidbody rigidbody in rigidbodiesOnPlatform)
+    //    {
+    //        MoveRigidbodiesOnPlatform(rigidbody);
+    //    }
+    //}
 
-    private void MoveRigidbodiesOnPlatform(Rigidbody rigidbody)
-    {
-        rigidbody.position += _platformRigidbody.position - _platformPositionLastFrame;
-    }
+    //private void MoveRigidbodiesOnPlatform(Rigidbody rigidbody)
+    //{
+    //    rigidbody.position += _platformRigidbody.position - _platformPositionLastFrame;
+    //}
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.attachedRigidbody != null)
-        {
-            if (!rigidbodiesOnPlatform.Contains(other.attachedRigidbody))
-                rigidbodiesOnPlatform.Add(other.attachedRigidbody);
-        }
+        //if (other.attachedRigidbody != null)
+        //{
+        //    if (!rigidbodiesOnPlatform.Contains(other.attachedRigidbody))
+        //        rigidbodiesOnPlatform.Add(other.attachedRigidbody);
+        //}
 
-        return;
+        //return;
         if (other.gameObject.CompareTag("Player"))
         {
             other.transform.parent = transform;
-            playerRB = other.GetComponent<Rigidbody>();
+            //playerRB = other.GetComponent<Rigidbody>();
         }
     }
     private void OnTriggerExit(Collider other)
     {
-        if (other.attachedRigidbody != null)
-        {
-            if (rigidbodiesOnPlatform.Contains(other.attachedRigidbody))
-                rigidbodiesOnPlatform.Remove(other.attachedRigidbody);
-        }
+        //if (other.attachedRigidbody != null)
+        //{
+        //    if (rigidbodiesOnPlatform.Contains(other.attachedRigidbody))
+        //        rigidbodiesOnPlatform.Remove(other.attachedRigidbody);
+        //}
 
-        return;
+        //return;
         if (other.gameObject.CompareTag("Player"))
         {
             other.transform.parent = null;
-            playerRB = null;
+            //playerRB = null;
         }
     }
 }
