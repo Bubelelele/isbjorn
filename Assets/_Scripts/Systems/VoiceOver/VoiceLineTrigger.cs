cd5750aa64549f19c334de159e2ce2cd15b160d2
istid1 <mhr97@hotmail.com>
2022-11-04 14:34:18 +0100

    Voicelines

--- Assets/_Scripts/Systems/VoiceOver/VoiceLineTrigger.cs
+++ Assets/_Scripts/Systems/VoiceOver/VoiceLineTrigger.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TriggerAudioTest : MonoBehaviour
+public class VoiceLineTrigger : MonoBehaviour
 {
     public AudioObject clipToBePlayed;
 
