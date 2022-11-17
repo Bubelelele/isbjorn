87ea21640fbae35a3ca27e02eeb9a367a22cccc5
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-11-18 00:01:24 +0100

    main menu looking good

--- Assets/_Scripts/_Unsorted/UI/Menu/MainMenu.cs
+++ Assets/_Scripts/_Unsorted/UI/Menu/MainMenu.cs
@@ -1,7 +1,27 @@
+using System;
 using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
-    
-    
+
+    [SerializeField] private PlayableDirector playableDirector;
+
+    private void Start() {
+        playableDirector.stopped += LoadScene;
+    }
+
+    public void Play() {
+        playableDirector.Play();
+    }
+
+    public void Quit() {
+        Application.Quit();
+    }
+
+    private void LoadScene(PlayableDirector director) {
+        playableDirector.stopped -= LoadScene;
+        SceneManager.LoadScene("Cycle 1");
+    }
 
 }
