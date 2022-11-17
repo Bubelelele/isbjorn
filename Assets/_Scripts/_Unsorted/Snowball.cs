1f148f979ae1e9c32b78848a78159fc23229ca3d
Granderous <kevin.b.grande@student.nord.no>
2022-11-17 17:06:41 +0100

    Snowball cooldown timer

--- Assets/_Scripts/_Unsorted/Snowball.cs
+++ Assets/_Scripts/_Unsorted/Snowball.cs
@@ -8,18 +8,49 @@ public class Snowball : MonoBehaviour
     public GameObject prefabSnowBall;
     public float throwForce;
     private bool contactWithPlayer;
+    public float waitTimer;
 
+    //private Animator playerAnim;
+    //AnimatorStateInfo animStateInfo;
+    //private float nTime;
 
+    //private bool animationFinished;
 
+    //private void Start()
+    //{
+    //    playerAnim = GameObject.Find("Player/RollPivot/Bear_Big").GetComponent<Animator>();
+    //}
 
 
     private void Update()
     {
+        //if (playerAnim.GetCurrentAnimatorStateInfo().IsName(""))
+        //{
+        //    playerAnim.
+        //}
+
+        //animStateInfo = playerAnim.GetCurrentAnimatorStateInfo(8);
+        //nTime = animStateInfo.normalizedTime;
+
+        //if (nTime > 1.0f)
+        //{
+        //    animationFinished = true;
+        //    Debug.LogError("Slash Finished");
+        //}
+
+
+        waitTimer -= Time.deltaTime;
+        if (waitTimer <= 0)
+        {
+            waitTimer = 0;
+        }
+
         if (contactWithPlayer)
         {
-            if (Mouse.current.leftButton.wasPressedThisFrame)
+            if (Mouse.current.leftButton.wasPressedThisFrame && waitTimer <= 0)
             {
                 Invoke("ThrowBall", 0.3f);
+                waitTimer = 2;
             }
         }
         
