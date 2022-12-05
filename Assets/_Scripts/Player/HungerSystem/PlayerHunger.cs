7ddfcbf790749605db2f586c97a0cb6a40c32466
tusjmod <64898919+tusjmod@users.noreply.github.com>
2022-12-05 15:26:38 +0100

    low food feedback

--- Assets/_Scripts/Player/HungerSystem/PlayerHunger.cs
+++ Assets/_Scripts/Player/HungerSystem/PlayerHunger.cs
@@ -1,54 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
+using MoreMountains.Feedbacks;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PlayerHunger : MonoBehaviour
-{
-    [Header("Player Hunger")]
-
-    public float maxHunger = 100;
+public class PlayerHunger : MonoBehaviour {
+    [Header("Player Hunger")] public float maxHunger = 100;
     public float hunger;
     public float hungerOverTime;
     public Slider hungerSlider;
+    public MMFeedbacks lowFoodFeedback;
+    public float lowFoodPoint = 20;
 
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         hungerSlider = FindObjectOfType<Slider>();
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         hunger = hunger - hungerOverTime * Time.deltaTime;
 
-        if (hunger <= 0)
-        {
+        if (hunger <= 0) {
             hunger = 0;
         }
-        if (hunger > 100)
-        {
-            hunger = 100; 
+
+        if (hunger > 100) {
+            hunger = 100;
         }
 
         UpdateSliders();
+        UpdateFeedback();
     }
 
-    public void AddFood(float Amount)
-    {
-        this.hunger += Amount; 
+    public void AddFood(float Amount) {
+        this.hunger += Amount;
     }
 
-    public void RemoveFood(float Amount)
-    {
+    public void RemoveFood(float Amount) {
         this.hunger -= Amount;
     }
 
 
-    void UpdateSliders()
-    {
+    void UpdateSliders() {
         if (hungerSlider != null)
-            hungerSlider.value = hunger / maxHunger; 
+            hungerSlider.value = hunger / maxHunger;
+    }
+
+    private void UpdateFeedback() {
+        if (lowFoodFeedback.IsPlaying) {
+            if (hunger >= 20) lowFoodFeedback.StopFeedbacks();
+        } else if (hunger < 20) {
+            lowFoodFeedback.PlayFeedbacks();
+        }
     }
 }
