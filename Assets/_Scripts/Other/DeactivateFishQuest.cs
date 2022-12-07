a4f9180e9913d09f3b9b77698891d246bc3fbbdd
Granderous <kevin.b.grande@student.nord.no>
2022-12-07 14:22:10 +0100

    Update DeactivateFishQuest.cs

--- Assets/_Scripts/Other/DeactivateFishQuest.cs
+++ Assets/_Scripts/Other/DeactivateFishQuest.cs
@@ -1,12 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class DeactivateFishQuest : MonoBehaviour
 {
     public GameObject fishCanvas;
-
-
 
     private void OnTriggerEnter(Collider other)
     {
