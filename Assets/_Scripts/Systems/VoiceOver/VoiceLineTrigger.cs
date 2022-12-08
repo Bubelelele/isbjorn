08a878d90731cb784c553ac8406b2e618009208c
istid1 <mhr97@hotmail.com>
2022-12-08 01:34:01 +0100

    Added dont eat every fish voiceline

--- Assets/_Scripts/Systems/VoiceOver/VoiceLineTrigger.cs
+++ Assets/_Scripts/Systems/VoiceOver/VoiceLineTrigger.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 
 public class VoiceLineTrigger : MonoBehaviour
 {
+	public GameObject fishInMouth;
     public AudioObject clipToBePlayed;
 	public bool onTriggerStay;
+	public bool notOnTriggerEnter;
+	public bool fishOnTriggerStay;
 	private bool isTriggered;
 	private bool waitForAudio = true;
 
-	private void OnTriggerEnter(Collider other)
+    private void Awake()
+    {
+		fishInMouth = GameObject.Find("Player/Bear_Big/Spine/Spine2/Spine3/Spine4/Neck1/Neck2/Jaw1/FishInMouth");
+	}
+    private void OnTriggerEnter(Collider other)
 	{
-		if (other.CompareTag("Player") && !isTriggered)
+		if (other.CompareTag("Player") && !isTriggered && !notOnTriggerEnter)
 		{
 			Vocals.instance.Say(clipToBePlayed);
 			isTriggered = true;
@@ -27,9 +34,12 @@ public class VoiceLineTrigger : MonoBehaviour
 			StartCoroutine("Wait", 0);
 			waitForAudio = false;
 		}
-	
-
-
+        if (other.CompareTag("Player") && fishOnTriggerStay && fishInMouth.activeSelf == true && !isTriggered)
+        {
+          
+			Vocals.instance.Say(clipToBePlayed);
+			isTriggered = true;
+        }
 	}
 	private IEnumerator Wait()
 	{
