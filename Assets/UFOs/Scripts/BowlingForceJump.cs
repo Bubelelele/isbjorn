957c0e596016fd7149d507c1e22cb34b6d24aea1
istid1 <mhr97@hotmail.com>
2022-12-08 16:16:50 +0100

    Moving stuff in folders

---
+++ Assets/UFOs/Scripts/BowlingForceJump.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class BowlingForceJump : MonoBehaviour
+{
+    public float pushForceJump;
+
+    private GameObject player;
+    private PlayerStateMachine playerStateMachine;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.Find("Player"); 
+        playerStateMachine = player.GetComponent<PlayerStateMachine>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && playerStateMachine.Input.Rolling)
+        {
+            Vector3 dir = player.transform.position - transform.position;
+            dir = dir.normalized;
+            player.GetComponent<Rigidbody>().AddForce((dir * pushForceJump), ForceMode.Impulse);
+        }
+    }
+}
